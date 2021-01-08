c7dbe1b9709ccc46a889bfa6b84330a2db3e0f0a
arijit <arijitnanditech@gmail.com>
2021-01-08 16:49:54 +0530

    python code to fetch real time data

---
+++ python/market-data.py
@@ -0,0 +1,37 @@
+# package import statement
+from smartapi.smartConnect import SmartConnect
+
+#create object of call
+obj=SmartConnect(api_key="UCiPVJlE")
+
+#login api call
+
+data = obj.generateSession("A351723","Arijit1989")
+refreshToken= data['data']['refreshToken']
+
+#fetch the feedtoken
+feedToken=obj.getfeedToken()
+
+## WebSocket
+from smartapi import WebSocket 
+FEED_TOKEN = feedToken
+CLIENT_CODE = "A351723"
+token="nse_cm|2885&nse_cm|1594&nse_cm|11536"
+
+ss = WebSocket(FEED_TOKEN, CLIENT_CODE)
+
+def on_tick(ws, tick):
+    print("Ticks: {}".format(tick))
+
+def on_connect(ws, response):
+    ws.send_request(token)
+
+def on_close(ws, code, reason):
+    ws.stop()
+
+# Assign the callbacks.
+ss.on_ticks = on_tick
+ss.on_connect = on_connect
+ss.on_close = on_close
+
+ss.connect( )
